5aba6148d9a11dcd1f4c99085e94293359e2bf89
michelearcopinto <guestphantom1@gmail.com>
2024-04-04 20:03:55 +0200

    feat: imported projects from laravel api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <script>
-import ExampleName from "./components/ExampleName.vue";
+import ProjectCard from "./components/ProjectCard.vue";
 import { store } from "./store";
 import axios from "axios";
 
 export default {
   components: {
-    ExampleName,
+    ProjectCard,
   },
   data() {
     return {
-      store,
+      arrayProjects: [],
+      currentPage: "",
+      lastPage: "",
     };
   },
-  created() {
-    this.requestRandomMail();
+  mounted() {
+    this.getProjects(1);
   },
   methods: {
-    requestRandomMail() {
+    getProjects(postApiPage) {
       axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .get(
+          "http://127.0.0.1:8000/api/test",
+
+          {
+            params: {
+              page: postApiPage,
+            },
+          }
+        )
         .then((result) => {
-          store.randomMail = result.data.response;
+          this.arrayProjects = result.data.projects.data;
+          this.currentPage = result.data.projects.current_page;
+          this.lastPage = result.data.projects.last_page;
+
+          console.log(this.currentPage, this.lastPage);
         });
     },
   },
@@ -28,10 +42,49 @@ export default {
 </script>
 
 <template>
-  <h1>Hello World</h1>
-  <h2>{{ store.helloValue }}</h2>
-  <h3>axios request: {{ store.randomMail }}</h3>
-  <ExampleName />
+  <h1>Stampa projects</h1>
+  <div class="container mt-5">
+    <div class="row">
+      <ProjectCard
+        v-for="(element, index) in arrayProjects"
+        :key="index"
+        :propElement="element"
+      />
+    </div>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination mx-auto">
+        <li class="page-item">
+          <button
+            class="page-link"
+            :class="{ disabled: currentPage === 1 }"
+            @click="getProjects(currentPage - 1)"
+          >
+            Previous
+          </button>
+        </li>
+
+        <li class="page-item" v-for="(element, index) in lastPage" :key="index">
+          <button
+            class="page-link"
+            :class="{ disabled: currentPage === element }"
+            @click="getProjects(element)"
+          >
+            {{ element }}
+          </button>
+        </li>
+
+        <li class="page-item">
+          <button
+            class="page-link"
+            :class="{ disabled: currentPage === lastPage }"
+            @click="getProjects(currentPage + 1)"
+          >
+            Next
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <style lang="scss">
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,71 @@
+<script>
+// import
+
+export default {
+  name: "ProjectCard",
+  props: ["propElement"],
+};
+</script>
+
+<template>
+  <div class="col-4 project-container">
+    <div class="card text-start p-3 project-card">
+      <figure v-show="propElement.cover_image">
+        <img
+          class="card-img-top"
+          :src="propElement.cover_image"
+          :alt="propElement.slug"
+        />
+      </figure>
+      <div class="card-body">
+        <h4 class="card-title">{{ propElement.title }}</h4>
+        <ul>
+          <li>
+            slug: <strong>{{ propElement.slug }}</strong>
+          </li>
+          <li v-if="propElement.category">
+            category:
+            <strong>{{ propElement.category.name }}</strong>
+          </li>
+          <li v-else>
+            category:
+            <strong>Nessuna</strong>
+          </li>
+          <li>
+            tags:
+            <ul v-if="propElement.tags.length > 0">
+              <li v-for="(element, index) in propElement.tags">
+                {{ element.name }}
+              </li>
+            </ul>
+            <span v-else><strong>Nessuno</strong></span>
+          </li>
+        </ul>
+        <p class="card-text">{{ propElement.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+h1 {
+  color: $font-color-second;
+  @include main-font-start;
+}
+
+li {
+  list-style-type: disc;
+}
+
+.project-container {
+  height: 450px;
+
+  .project-card {
+    height: 100%;
+    overflow: auto;
+  }
+}
+</style>
